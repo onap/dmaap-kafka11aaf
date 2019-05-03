f9d989edac72d9b49ee9790d7e97eeda97551cf5
sunil.unnava <sunil.unnava@att.com>
2019-05-03 16:27:49 -0400

    AAF certificate update changes
    Issue-ID: DMAAP-1192
    Change-Id: I1496276a5b7b9d681c122e1e8ad1e4908da1fa74
    Signed-off-by: sunil.unnava <sunil.unnava@att.com>

--- src/main/docker/start-kafka.sh
+++ src/main/docker/start-kafka.sh
@@ -135,8 +135,8 @@ if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
   eval $CUSTOM_INIT_SCRIPT
 fi
 cp /tmp/kafka11aaf-jar-with-dependencies.jar $KAFKA_HOME/libs
-cp /tmp/keyfilenew  $KAFKA_HOME/config
-cp /tmp/truststoreONAPall.jks $KAFKA_HOME/config
+cp /tmp/org.onap.dmaap.mr.keyfile  $KAFKA_HOME/config
+cp /tmp/org.onap.dmaap.mr.trust.jks $KAFKA_HOME/config
 cp /tmp/org.onap.dmaap.mr.p12 $KAFKA_HOME/config
 cp /tmp/kafka_server_jaas.conf $KAFKA_HOME/config
 cp /tmp/cadi.properties $KAFKA_HOME/config
