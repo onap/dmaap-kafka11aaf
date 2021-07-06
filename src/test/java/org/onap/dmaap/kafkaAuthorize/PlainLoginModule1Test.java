0fee92a5e399bb57fdfce0c24686d4eb74b47e95
seanfos <sean.osullivan@est.tech>
2021-07-06 16:44:29 +0100

    [DMAAP-KAFKA] Kafka Upgrade
    Signed-off-by: seanfos <sean.osullivan@est.tech>
    Change-Id: Ic83906004ca15494187972c9d8fd6a437766de85
    Issue-ID: DMAAP-209

--- src/test/java/org/onap/dmaap/kafkaAuthorize/PlainLoginModule1Test.java
+++ src/test/java/org/onap/dmaap/kafkaAuthorize/PlainLoginModule1Test.java
@@ -26,10 +26,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"jdk.internal.reflect.*"})
 @PrepareForTest({ PlainLoginModule1.class })
 public class PlainLoginModule1Test {
 
